f961612f5471740d5cad1f30a73066c283d7bce6
johnnygomes25 <107089229+johnnygomes25@users.noreply.github.com>
2022-10-07 22:25:41 -0300

    Exception handling - delete

--- src/main/java/com/educandoweb/curso/resources/exception/ResouceExceptionHandler.java
+++ src/main/java/com/educandoweb/curso/resources/exception/ResouceExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.educandoweb.curso.services.exeption.DataBaseException;
 import com.educandoweb.curso.services.exeption.ResourceNotFoundException;
 
 @ControllerAdvice
@@ -22,9 +23,19 @@ public class ResouceExceptionHandler {
 		StandardErro err = new StandardErro(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 		
+	
+}
+	@ExceptionHandler(DataBaseException.class)
+	public ResponseEntity<StandardErro> database(DataBaseException e, HttpServletRequest request){
+		String error = "Database error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardErro err = new StandardErro(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+		
 	}
 	
-	
+}
+
 	
 
-}
+
---
+++ src/main/java/com/educandoweb/curso/services/exeption/DataBaseException.java
@@ -0,0 +1,13 @@
+package com.educandoweb.curso.services.exeption;
+
+public class DataBaseException extends RuntimeException {
+
+	
+	private static final long serialVersionUID = 1L;
+	
+	public DataBaseException(String msg) {
+		super(msg);
+	}
+}
+
+
